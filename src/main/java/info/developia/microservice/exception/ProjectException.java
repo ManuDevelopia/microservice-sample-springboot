a982b462132b12a942ac4fa87acf9b0dbbf4f2c8
manudevelopia <manugarciarodriguez@gmail.com>
2017-07-02 11:30:53 +0200

    Add Project exception

---
+++ src/main/java/info/developia/microservice/exception/ProjectException.java
@@ -0,0 +1,22 @@
+package info.developia.microservice.exception;
+
+
+public class ProjectException extends Exception {
+
+  public ProjectException () {
+
+  }
+
+  public ProjectException (String message) {
+    super(message);
+  }
+
+  public ProjectException (Throwable cause) {
+    super(cause);
+  }
+
+  public ProjectException ( String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
